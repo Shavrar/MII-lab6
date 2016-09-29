4b1c8e472a2807ebe1ec57fcc057d382ba3492f9
Turlei Roman <r.turlei.rt@gmail.com>
2016-09-29 23:14:06 +0300

    who is your daddy?

--- lab6/Program.cs
+++ lab6/Program.cs
@@ -22,52 +22,132 @@ namespace lab6
             ItemId = temp[1].Split(',').Select(int.Parse).ToList();
         }
 
-        public bool ContainItems(Record item)
+        public bool ContainRecordItems(Record record)
         {
-            return item.ItemId.All(id => this.ItemId.Contains(id));
+            return record.ItemId.All(id => this.ItemId.Contains(id));
+        }
+        public bool ContainItems(List<int> items)
+        {
+            return items.All(id => this.ItemId.Contains(id));
         }
     }
 
+   
     class Program
     {
+        public static bool DictionaryContainsList<T>(Dictionary<int, List<T>> dictionary, List<T> value)
+        {           
+            foreach (var list in dictionary)
+            {
+                if (ListRawEquel(list.Value, value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool ListRawEquel<T>(List<T> list1, List<T> list2)
+        {
+            if (list1.Count != list2.Count) return false;
+            foreach (var item in list1)
+            {
+                if (!list2.Contains(item))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool ListContainsList<T>(List<List<T>> lisy, List<T> value)
+        {
+            var result = false;
+            foreach (var list in lisy)
+            {
+                result = true;
+                result = ListRawEquel(list, value);
+                if (result) break;
+            }
+            return result;
+        }
+
+        public static List<List<T>> GetAllInstances<T>(List<T> list, List<List<T>> result, List<T> currentNode)
+        {
+            if (currentNode == null)
+            {
+                foreach (var item in list)
+                {
+                    var node = new List<T> {item};
+                    var temp = new List<T>(list);
+                    temp.Remove(item);
+                    if (!ListContainsList(result, node))
+                    {
+                        result.Add(node);
+                    }
+                    result = GetAllInstances(temp, result, node);
+                }
+            }
+            else
+            {
+                foreach (var item in list)
+                {
+                    var node = new List<T>(currentNode);
+                    node.Add(item);
+                    var temp = new List<T>(list);
+                    temp.Remove(item);
+                    if (!ListContainsList(result, node))
+                    {
+                        result.Add(node);
+                    }
+                    result = GetAllInstances(temp, result, node);
+                }
+            }
+            return result;
+        }
+
         static void Main(string[] args)
         {
             var records = LoadRecords();
 
-            var recordsSort = records.GroupBy(a => a.Id).Select(g => g.ToList().OrderBy(a => a.Time));
-            var counter = new Dictionary<int, List<int>>();
-
-            foreach (var item in recordsSort)
+            var recordsSorted = records.GroupBy(a => a.Id).Select(g => g.ToList().OrderBy(a => a.Time));
+            var allInstancesStorage = new List<List<int>>();
+            foreach (var group in recordsSorted)
             {
-                foreach (var record in item)
+                foreach (var record in group)
                 {
-                    int cot = 1;
-                    if (!counter.ContainsValue(record.ItemId))
+                    allInstancesStorage = GetAllInstances(record.ItemId, allInstancesStorage, null);
+                }
+            }
+            var instancesStorage = new Dictionary<int, List<int>>();
+            var numberOfGroups = (double)recordsSorted.Count();
+            foreach (var instance in allInstancesStorage)
+            {
+                int i = 0;
+                foreach (var group in recordsSorted)
+                {
+                    foreach (var record in group)
                     {
-                        foreach (var t in recordsSort)
+                        if (record.ContainItems(instance))
                         {
-                            if (t.Any(b => b.ContainItems(record)))
-                            {
-                                cot++;
-                            }
-                            
+                            i++;
+                            break;
                         }
                     }
-                    if ((cot*1.0)/recordsSort.Count() >= 0.4)
-                    {
-                        counter.Add(counter.Count,record.ItemId);
-                    }
                 }
-                
+                if (i/numberOfGroups >= 0.4)
+                {
+                    instancesStorage.Add(instancesStorage.Count,instance);
+                }
             }
 
-            counter.ToList().ForEach(a =>
+
+            instancesStorage.ToList().ForEach(a =>
             {
                 var q = "";
                 a.Value.ForEach(f => q+=$"{f}, ");
                 Console.WriteLine($"{a.Key} {q}");
             });
-
 
             Console.ReadKey();
         }
@@ -99,7 +179,7 @@ namespace lab6
         public static List<Record> LoadRecords()
         {
             var records = new List<Record>();
-            string path = @"C:\temp\file.txt";
+            string path = @"D:\file.txt";
             FileInfo fileInf = new FileInfo(path);
             if (fileInf.Exists)
             {
