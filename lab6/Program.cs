d584f9d5e97cc3ae3c1e8a5e17c2c9ba4efc5c05
Turlei Roman <r.turlei.rt@gmail.com>
2016-09-30 01:53:44 +0300

    Who is fucking middle .net developer?

--- lab6/Program.cs
+++ lab6/Program.cs
@@ -35,6 +35,30 @@ namespace lab6
    
     class Program
     {
+        //checks if array contain other array
+        public static bool EnumerableContainsWithOrder<T>(T[] origin,T[] array)
+        {
+            var last = 0;
+            foreach (var a in array)
+            {
+                var check = true;
+                for (var i = last; i < origin.Length; i++)
+                {
+                    if (origin[i].Equals(a))
+                    {
+                        check = false;
+                        last = i + 1;
+                        break;
+                    }
+                }
+                if (check)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        //Check if dictionary of lists contains certain list
         public static bool DictionaryContainsList<T>(Dictionary<int, List<T>> dictionary, List<T> value)
         {           
             foreach (var list in dictionary)
@@ -47,6 +71,7 @@ namespace lab6
             return false;
         }
 
+        //Check if list has equels values to equels in second list
         public static bool ListRawEquel<T>(List<T> list1, List<T> list2)
         {
             if (list1.Count != list2.Count) return false;
@@ -60,6 +85,7 @@ namespace lab6
             return true;
         }
 
+        //Check if list of lists contains certain list
         public static bool ListContainsList<T>(List<List<T>> lisy, List<T> value)
         {
             var result = false;
@@ -72,6 +98,7 @@ namespace lab6
             return result;
         }
 
+        //Returns all unique combinations of values of list 
         public static List<List<T>> GetAllInstances<T>(List<T> list, List<List<T>> result, List<T> currentNode)
         {
             if (currentNode == null)
@@ -106,12 +133,68 @@ namespace lab6
             return result;
         }
 
+        public static List<int[]> GetFinale(List<int[]> data, List<int[]> result, int[] current, int count)
+        {
+            if (current == null)
+            {
+                for (var i = 1; i <= count; i++)
+                {
+                    var cur = new int[] {i};
+                    result.Add(cur);
+                    result = GetFinale(data, result, cur, count);
+                }
+            }
+            else
+            {
+                for (int i = 1; i <= count; i++)
+                {
+                    var cur = current.Concat(new[] {i}).ToArray();
+                    double check = 0;
+                    foreach (var array in data)
+                    {
+                        if (EnumerableContainsWithOrder(array, cur))
+                        {
+                            check++;
+                        }
+                    }
+
+                    if (check/data.Count>=0.4)
+                    {
+                        result.Add(cur);
+                        result = GetFinale(data, result, cur, count);
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static List<int[]> DeleteEquelities(List<int[]> result)
+        {
+            foreach (var array in result)
+            {
+                foreach (var ar in result)
+                {
+                    if (ar!=array && EnumerableContainsWithOrder(ar,array))
+                    {
+                        var temp = new List<int[]>(result);
+                        temp.Remove(array);
+                        return DeleteEquelities(temp);
+                    }
+                }
+            }
+            return result;
+        }
+
         static void Main(string[] args)
         {
-            var records = LoadRecords();
+            var records = LoadRecords();// geting all records
 
-            var recordsSorted = records.GroupBy(a => a.Id).Select(g => g.ToList().OrderBy(a => a.Time));
-            var allInstancesStorage = new List<List<int>>();
+            var recordsSorted = records.GroupBy(a => a.Id).Select(g => g.ToList().OrderBy(a => a.Time)); // devided them by user id and sorted by date
+
+            var allInstancesStorage = new List<List<int>>();// list for all combinations
+
+            //filling this shit with data
             foreach (var group in recordsSorted)
             {
                 foreach (var record in group)
@@ -119,8 +202,10 @@ namespace lab6
                     allInstancesStorage = GetAllInstances(record.ItemId, allInstancesStorage, null);
                 }
             }
-            var instancesStorage = new Dictionary<int, List<int>>();
-            var numberOfGroups = (double)recordsSorted.Count();
+            var instancesStorage = new Dictionary<int, List<int>>();//this is where we store valid combinations(support >= 0.4)
+
+            var numberOfGroups = (double)recordsSorted.Count();//users count
+
             foreach (var instance in allInstancesStorage)
             {
                 int i = 0;
@@ -137,21 +222,88 @@ namespace lab6
                 }
                 if (i/numberOfGroups >= 0.4)
                 {
-                    instancesStorage.Add(instancesStorage.Count,instance);
+                    instancesStorage.Add(instancesStorage.Count+1,instance);
                 }
             }
 
-
-            instancesStorage.ToList().ForEach(a =>
+            Console.WriteLine("------------------------------------------------");
+            foreach (var item in instancesStorage)
             {
-                var q = "";
-                a.Value.ForEach(f => q+=$"{f}, ");
-                Console.WriteLine($"{a.Key} {q}");
-            });
+                Console.Write(item.Key+" |");
+                item.Value.ForEach(a => Console.Write(a + " "));
+                Console.WriteLine();
+            }
+            Console.WriteLine("------------------------------------------------");
+            var newRecordsStore = new Dictionary<int, List<List<int>>>();// for changed list of records
+
+            foreach (var group in recordsSorted)
+            {
+                foreach (var record in group)
+                {
+                   var temp = new List<int>();
+                    foreach (var instance in instancesStorage)
+                    {
+                        if (record.ContainItems(instance.Value))
+                        {
+                            temp.Add(instance.Key);
+                        }
+                    }
+                    if (temp.Any())
+                    {
+                        if (newRecordsStore.ContainsKey(record.Id))
+                        {
+                            newRecordsStore[record.Id].Add(temp);
+                        }
+                        else
+                        {
+                            newRecordsStore.Add(record.Id,new List<List<int>> {temp});
+                        }
+                    }
+                }
+            }
+            foreach (var item in newRecordsStore)
+            {
+                Console.Write("User "+item.Key + " :");
+                foreach (var list in item.Value)
+                {
+                    Console.Write("{");
+                    list.ForEach(a => Console.Write(a+","));
+                    Console.Write("}");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("------------------------------------------------");
+            var data = new List<int[]>();
+            foreach (var item in newRecordsStore)
+            {
+                var list = new List<int>();
+                foreach (var a in item.Value)
+                {
+                    list.AddRange(a);
+                }
+                data.Add(list.ToArray());
+            }
+            
+            var result = new List<int[]>();
+
+            result = GetFinale(data, result, null, instancesStorage.Count);
+
+            result = DeleteEquelities(result);
+
+            foreach (var array in result)
+            {
+                Console.Write("<");
+                foreach (var item in array)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.Write(">");
+                Console.WriteLine();
+            }
 
             Console.ReadKey();
         }
-
+        //new records input
         public static List<Record> GetRecords()
         {
             var records = new List<Record>();
@@ -165,7 +317,7 @@ namespace lab6
             }
             return records;
         }
-
+        //shows records
         public static void Show(List<Record> records)
         {
             foreach (var record in records)
@@ -175,7 +327,7 @@ namespace lab6
                 Console.WriteLine();
             }
         }
-
+        //loads records or initialize creation of new records
         public static List<Record> LoadRecords()
         {
             var records = new List<Record>();
